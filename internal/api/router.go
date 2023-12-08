2952f4adb2323187eaf65a6563d07c6ab3686c96
逆流而上 <1666888816@qq.com>
2023-12-08 18:02:17 +0800

    feat: 引入cago开始开发

---
+++ internal/api/router.go
@@ -0,0 +1,16 @@
+package api
+
+import (
+	"context"
+	"github.com/codfrm/cago/server/mux"
+)
+
+// Router 路由
+// @title    api文档
+// @version  1.0
+// @BasePath /api/v1
+func Router(ctx context.Context, root *mux.Router) error {
+	// 注册储存实例
+
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"github.com/DokiDoki1103/ks/internal/api"
+	"github.com/codfrm/cago/database/db"
+	"github.com/codfrm/cago/examples/simple/migrations"
+	"github.com/codfrm/cago/pkg/component"
+	"log"
+
+	"github.com/codfrm/cago"
+	"github.com/codfrm/cago/configs"
+	"github.com/codfrm/cago/server/mux"
+)
+
+func main() {
+	ctx := context.Background()
+	cfg, err := configs.NewConfig("simple")
+	if err != nil {
+		log.Fatalf("load config err: %v", err)
+	}
+	err = cago.New(ctx, cfg).
+		Registry(component.Core()).
+		Registry(component.Database()).
+		Registry(component.Broker()).
+		Registry(component.Redis()).
+		Registry(component.Cache()).
+		Registry(cago.FuncComponent(func(ctx context.Context, cfg *configs.Config) error {
+			return migrations.RunMigrations(db.Default())
+		})).
+		RegistryCancel(mux.HTTP(api.Router)).
+		Start()
+	if err != nil {
+		log.Fatalf("start err: %v", err)
+		return
+	}
+}
